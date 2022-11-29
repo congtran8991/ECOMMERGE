3422284b80bbba4fe531a7aee164d5943285d645
congtran8991 <cong.tran@deliveree.com>
2022-11-29 16:21:37 +0700

    update

--- projectname/src/api/category/controllers/category.js
+++ projectname/src/api/category/controllers/category.js
@@ -8,7 +8,7 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::category.category', () => ({
     async findByCategory(ctx, next){
-        ctx.send({msg: "hdvhbdhvdbvbhd"})
+        ctx.send({msg: "hdvhbdhvdbvbhdd"})
     },
     async findByCategory1(ctx, next){
         const { data, meta } = await super.find(ctx);
